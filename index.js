8b89274e508bf91ba025f2620b9b189453e0442f
AnubhavSrivastavaGithub <rajeshbigbomb@gmail.com>
2021-06-24 20:14:43 +0530

    PracticeJavaScript

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+var fruits = ["apple","mango","peach","strawberry"];
+
 function message(msg){
-    document.getElementById("test").innerHTML = msg ;
+    document.getElementById("test").innerHTML = fruits ;
 }
+
+function loadfruit(){
+    document.getElementById("fruit").innerHTML = fruits;
+}
+
+function loop(){
+    var x = prompt("Enter a number :");
+    for(var i=0;i<x;i++){
+        if(i%2 === 0)
+        document.write(i);
+        else 
+        document.write('This is odd');
+    }
+}
+
+
